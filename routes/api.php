270a1ef1c7396272b042785c420526462ab6f2fb
meshu-dev <admin@requiredev.com>
2020-07-13 20:09:48 +0100

    Moved routes to API from web

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,34 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/', 'IndexController@index');
+
+Route::group(
+	['middleware' => 'api'],
+	function ($router) {
+	    Route::group(['prefix' => 'auth'], function () {
+		    Route::post('login', 'AuthController@login');
+		    Route::post('logout', 'AuthController@logout');
+		    Route::post('refresh', 'AuthController@refresh');
+		    Route::post('me', 'AuthController@me');
+	    });
+
+	    Route::group(['prefix' => 'job-sites'], function () {
+	    	$controller = 'JobSiteController';
+		    Route::post('/', "$controller@create");
+		    Route::get('/', "$controller@readAll");
+		    Route::get('/{id}', "$controller@read");
+		    Route::put('/{id}', "$controller@update");
+		    Route::delete('/', "$controller@delete");
+	    });
+
+	    Route::group(['prefix' => 'jobs'], function () {
+	    	$controller = 'JobController';
+		    Route::post('/', "$controller@create");
+		    Route::get('/', "$controller@readAll");
+		    Route::get('/{id}', "$controller@read");
+		    Route::put('/{id}', "$controller@update");
+		    Route::delete('/', "$controller@delete");
+	    });
+	}
+);
--- routes/web.php
+++ routes/web.php
@@ -10,35 +10,3 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', 'IndexController@index');
-
-Route::group(
-	['middleware' => 'api'],
-	function ($router) {
-	    Route::group(['prefix' => 'auth'], function () {
-		    Route::post('login', 'AuthController@login');
-		    Route::post('logout', 'AuthController@logout');
-		    Route::post('refresh', 'AuthController@refresh');
-		    Route::post('me', 'AuthController@me');
-	    });
-
-	    Route::group(['prefix' => 'job-sites'], function () {
-	    	$controller = 'JobSiteController';
-		    Route::post('/', "$controller@create");
-		    Route::get('/', "$controller@readAll");
-		    Route::get('/{id}', "$controller@read");
-		    Route::put('/{id}', "$controller@update");
-		    Route::delete('/', "$controller@delete");
-	    });
-
-	    Route::group(['prefix' => 'jobs'], function () {
-	    	$controller = 'JobController';
-		    Route::post('/', "$controller@create");
-		    Route::get('/', "$controller@readAll");
-		    Route::get('/{id}', "$controller@read");
-		    Route::put('/{id}', "$controller@update");
-		    Route::delete('/', "$controller@delete");
-	    });
-	}
-);
