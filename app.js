d81cb4336e31be078660e7d449e350e598e66fe3
meshu-dev <harmeshuppal@gmail.com>
2022-11-28 20:05:13 +0000

    Added util classes

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
+#!/usr/bin/env node
+
+// Load config params to process.env
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const mongoDb = require('./src/utils/mongoDb');
+const port = process.env.APP_PORT || 3000;
+
+// Setup CORS to grant access to frontend website
+app.use(function (req, res, next) {
+  let allowedOrigins = [
+      process.env.APP_FRONTEND_SITE,
+      process.env.APP_ADMIN_SITE,
+    ],
+    origin = req.headers.origin;
+
+  if (origin && allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Total-Count'
+  );
+  res.header('Access-Control-Expose-Headers', 'X-Total-Count');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+mongoDb.connect();
 
 // Create and setup routes
 let routePath = './src/routes',
