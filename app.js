23abb84bc15f96c4ef46987aff71dd37bc44d714
meshu-dev <harmeshuppal@gmail.com>
2022-11-30 17:45:02 +0000

    Add debug for env variables

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const mongoDb = require('./src/utils/mongoDb');
 const tokenAuth = require('./src/utils/auth');
 const port = process.env.APP_PORT || 3000;
 
+console.log('process.env', process.env);
+
 // Setup CORS to grant access to frontend website
 app.use(function (req, res, next) {
   const allowedOrigins = [
