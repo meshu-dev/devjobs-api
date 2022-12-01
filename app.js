ba25470429df88fb19cf95c4dbf936b5aba8d59b
meshu-dev <harmeshuppal@gmail.com>
2022-12-01 11:02:46 +0000

    Cors setup

--- app.js
+++ app.js
@@ -41,7 +41,24 @@ app.use(function (req, res, next) {
   }
 }); */
 
-app.use(cors());
+let whitelist = [
+  process.env.APP_FRONTEND_SITE,
+  process.env.APP_ADMIN_SITE
+];
+
+let corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.use(cors(corsOptionsDelegate));
 
 // Parse JSON data in requests
 app.use(express.json());
